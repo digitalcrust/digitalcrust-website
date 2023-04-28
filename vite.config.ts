290368bcda58cd00e9d7d3f34ff0a9a65acd10b0
Daven Quinn <dev@davenquinn.com>
2023-04-28 04:40:10 -0500

    Moved pages a bit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
-import { UserConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import ssr from "vite-plugin-ssr/plugin";
+import mdx from "@mdx-js/rollup";
+import wikiLinks from "remark-wiki-link";
+import { UserConfig } from "vite";
 
 const config: UserConfig = {
-  plugins: [react(), ssr()]
-}
+  plugins: [react(), mdx({ remarkPlugins: [wikiLinks] }), ssr()],
+};
 
-export default config
+export default config;
